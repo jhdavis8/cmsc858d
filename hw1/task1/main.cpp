1dc39ebd5f0cf9fb67957ac4994ecd3ba7908a33
Josh Davis <jhdavis@umd.edu>
2022-03-06 19:12:38 -0500

    Rank support now working, passes basic unit tests

--- hw1/task1/main.cpp
+++ hw1/task1/main.cpp
@@ -19,10 +19,22 @@ int main() {
   int passes = 0, tests = 0;
 
   bitvector b;
-  b.assign({0,1,1,1,0,1,0,1,1,1,0,0,1,1,0,1,0,1,1,1,0,1,1,1,0,0,0,0});
+  b.assign({0,1,1,1,0,1,0,1,1,1,0,0,1,1,0,1,0,1,1,1,0,1,1,1,0,0,0,0,1,1,1,0,1,0,0,1,1,0,1,0,1,0,1,1,0,0,1,1,1,0,0,0,1,0,1,1,0,0,0,1,0,1,0});
   rank_support r(b);
+  test_eq(r(0), 0, "0", tests, passes);
+  test_eq(r(1), 1, "1", tests, passes);
+  test_eq(r(4), 3, "4", tests, passes);
+  test_eq(r(8), 6, "8", tests, passes);
+  test_eq(r(19), 13, "19", tests, passes);
+  test_eq(r(35), 21, "35", tests, passes);
+  test_eq(r(36), 22, "36", tests, passes);
+  test_eq(r(37), 22, "37", tests, passes);
+  test_eq(r(43), 26, "43", tests, passes);
 
   printf("Test complete. %f%% pass rate, %d pass, %d fail, %d total.\n",
-         (double) passes/tests, passes, tests - passes, tests);
+         (double) (passes/tests)*100, passes, tests - passes, tests);
+
+  r.print_members();
+  
   return 0;
 }
--- hw1/task1/rank_support.hpp
+++ hw1/task1/rank_support.hpp
@@ -4,42 +4,87 @@
 #include <string>
 #include <cstdio>
 #include <cmath>
+#include <bit>
 #include <compact_vector.hpp>
 
-#define bitvector compact::vector<unsigned,1>
-#define comvector compact::vector<unsigned>
+#define bitvector compact::vector<unsigned long,1>
+#define comvector compact::vector<unsigned long>
 
 class rank_support {
-  std::size_t n;
-  std::size_t s;
-  std::size_t b;
+  uint64_t n;
+  uint64_t s;
+  uint64_t b;
   const bitvector& bv;
   comvector& rs;
   comvector& rb;
-  comvector& rp;
+  //comvector& rp;
   
  public:
   rank_support(const bitvector& b_in)
-      : n(b_in.size()), bv(b_in), s(ceil(pow(log2(n), 2) / 2)),
+      : n(b_in.size()), bv(b_in), s(pow(ceil(log2(n)), 2)),
         rs(*(new comvector(ceil(log2(n)), ceil(n / s)))),
-        b(ceil(log2(n) / 2)),
-        rb(*(new comvector(ceil(log2(s)), ceil(n / b)))),
-        rp(*(new comvector(ceil(log2(b)), pow(2, b)*b))) {
+        b(ceil(log2(n))),
+        rb(*(new comvector(ceil(log2(s)), ceil(n / b))))
+        /*,rp(*(new comvector(ceil(log2(b)), pow(2, b)*b)))*/
+  {
     auto scan = std::vector<unsigned long>(n);
-    for (int i = 0; i < n; i++) {
+    for (uint64_t i = 0; i < n; i++) {
       scan[i] = bv[i] + ((i == 0) ? 0 : scan[i-1]);
       if (i % s == 0) rs[i/s] = ((i == 0) ? 0 : scan[i-1]);
       if (i % b == 0) rb[i/b] = ((i == 0) ? 0 : scan[i-1] - rs[i/s]);
     }
-    for (int i = 0; i < pow(2, b); i++) {
-      for (int j = 0; j < b; j++) {
-        rp[(i*b)+j] = (i % int(pow(2, b - j)))/pow(2, b - 1 - j) + (j == 0 ? 0 : rp[(i*b)+(j-1)]);
+    /*
+    for (uint64_t i = 0; i < pow(2, b); i++) {
+      for (uint64_t j = 0; j < b; j++) {
+        rp[(i*b)+j] = (i % ((uint64_t) pow(2, b - j)))/pow(2, b - 1 - j) + (j == 0 ? 0 : rp[(i*b)+(j-1)]);
       }
     }
+    */
   }
+
+  uint64_t rank1(uint64_t ind) {
+    if (ind >= n) printf("ERROR: invalid index to call rank on\n");
+    uint64_t my_s = floor(ind / s);
+    uint64_t my_b = floor(ind / b);
+    uint64_t rank_up_to_block = rs[my_s] + rb[my_b];
+    uint64_t block_start = my_b*b;
+    uint64_t block_content = 0;
+    for (uint64_t i = 0; i <= ind - block_start; i++) {
+      block_content |= bv[block_start+i] << i;
+    }
+    return rank_up_to_block + std::popcount(block_content);
+  }
+
+  uint64_t operator()(uint64_t ind) {
+    return this->rank1(ind);
+  }
+
+  void print_members() {
+    for (uint64_t i = 0; i < n; i++) {
+      printf("%lu ", bv[i]);
+    }
+    printf("\n");
+    for (uint64_t i = 0; i < n; i++) {
+      if (i % s == 0) printf("%lu ", (unsigned long) rs[i/s]);
+      else printf("  ");
+    }
+    printf("\n");
+    for (uint64_t i = 0; i < n; i++) {
+      if (i % b == 0) printf("%lu ", (unsigned long) rb[i/b]);
+      else printf("  ");
+    }
+    printf("\n\n");
+    /*
+    for (uint64_t i = 0; i < pow(2, b); i++) {
+      for (uint64_t j = 0; j < b; j++) {
+        printf("%lu ", (unsigned long) rp[(i*b)+j]);
+      }
+      printf("\n");
+    }
+    */
+  }
+  
   /*
-  uint64_t rank1(uint64_t i);
-  uint64_t operator()(uint64_t i);
   uint64_t overhead();
   void save(std::string& fname);
   void load(std::string& fname);
